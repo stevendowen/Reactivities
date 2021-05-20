0f01bf6abb3f94296b44e8c8e0bce66e1b38d02b
Steven Owen <owen.steven.d@gmail.com>
2021-05-20 16:06:26 -0600

    Added React Router, seperated form and detail page

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,33 +1,34 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 // import axios from 'axios';
 import { Container } from 'semantic-ui-react';
 import NavBar from './NavBar';
 import ActivityDashboard from '../../features/activities/dashboard/ActivityDashboard';
-import LoadingComponent from './LoadingComponent';
-import { useStore } from '../stores/store';
 import { observer } from 'mobx-react-lite';
+import HomePage from '../../features/home/HomePage';
+import { Route, useLocation } from 'react-router-dom';
+import ActivityForm from '../../features/activities/form/ActivityForm';
+import ActivityDetails from '../../features/activities/details/ActivityDetails';
 
 
 function App() {
-  const {activityStore} = useStore();
-
-  useEffect(() => {
-    activityStore.loadActivities();
-    // *** This is getting data from API 
-    // axios.get<Activity[]> ('http://localhost:5000/api/activities').then(response => {
-    //   setActivities(response.data);
-    // })
-  }, [activityStore]);
-
-  if(activityStore.loadingInitial) return <LoadingComponent content='Loading app' />
-
+  const location = useLocation();
 // <> is the same as <Fragment></Fragment>
   return (
     <>
-      <NavBar />
-      <Container style={{marginTop: '7em'}}>
-        <ActivityDashboard />
-      </Container>
+      <Route exact path='/' component={HomePage} />
+      <Route 
+        path={'/(.+)'}
+        render={() => (
+          <>
+            <NavBar />
+            <Container style={{marginTop: '7em'}}>
+              <Route exact path='/activities' component={ActivityDashboard} />
+              <Route path='/activities/:id' component={ActivityDetails} />
+              <Route key={location.key} path={['/createActivity', '/manage/:id']} component={ActivityForm} /> 
+            </Container>
+          </>
+        )}
+      />
 
     </>
   );
--- client-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -1,26 +1,32 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Grid } from 'semantic-ui-react'
 import Activitylist from './ActivityList'
-import ActivityDetails from '../details/ActivityDetails'
-import ActivityForm from '../form/ActivityForm'
+
 import { useStore } from '../../../app/stores/store'
 import { observer } from 'mobx-react-lite'
+import LoadingComponent from '../../../app/layout/LoadingComponent'
 
 
 export default observer(function ActivityDashboard() {
-
     const { activityStore } = useStore();
-    const { selectedActivity, editMode } = activityStore;
+    const { loadActivities, activityRegistry } = activityStore;
+
+useEffect(() => {
+    if (activityRegistry.size <= 1) loadActivities();
+    // *** This is getting data from API 
+    // axios.get<Activity[]> ('http://localhost:5000/api/activities').then(response => {
+    //   setActivities(response.data);
+    // })
+}, [activityRegistry.size, loadActivities]);
+
+if(activityStore.loadingInitial) return <LoadingComponent content='Loading app' />
     return (
         <Grid>
             <Grid.Column width='10'>
                 <Activitylist />
             </Grid.Column>
             <Grid.Column width='6'>
-                {selectedActivity && !editMode &&
-                <ActivityDetails />}
-                {editMode &&
-                <ActivityForm />}
+                <h2>Activity Filters</h2>
             </Grid.Column>
         </Grid>
     )
