60a57bd496b41b6f33b7771c8675de8e354e1286
Steven Owen <owen.steven.d@gmail.com>
2021-05-25 16:03:40 -0600

    Added API Identity/Jwt Tokens/Auth with users

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -2,6 +2,7 @@ using System;
 using System.Threading.Tasks;
 using Application.Activities;
 using Domain;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers
